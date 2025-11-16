819f5cd4d469d2eae163757491f3b3c1b3ccd914
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:32:07 +0530

    Create logicalnotexample.java

---
+++ java module 1/java module 2/logicalnotexample.java
@@ -0,0 +1,12 @@
+public class LogicalNotExample {
+    public static void main(String[] args) {
+        boolean isRaining = false;
+
+        if(!isRaining){
+            System.out.println("You can go outside without umbrella");
+        }
+        else{
+            System.out.println("Take an Umbrella");
+        }
+    }
+}
