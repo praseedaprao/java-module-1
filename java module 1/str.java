08861c3c6612f93731c8b44ccee3bc9d77c5f715
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:15:11 +0530

    Create str.java

---
+++ java module 1/str.java
@@ -0,0 +1,17 @@
+public class Str {
+    public static void main(String[] args) {
+        String a = "Hello";
+        String b = "hello";
+
+        System.out.println(a.toLowerCase());
+        System.out.println(a.toUpperCase());
+        System.out.println(a.length());
+        System.out.println(a.charAt(0));
+
+        if (a.equalsIgnoreCase(b)) {
+            System.out.println("Both the strings are equal");
+        } else {
+            System.out.println("They are not equal");
+        }
+    }
+}
