b3b56ea8f37671f312e5be0736d2694ee249d5b2
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:27:04 +0530

    Create bitwisenotexample.java

---
+++ java module 1/java module 2/bitwisenotexample.java
@@ -0,0 +1,7 @@
+public class BitWiseNotExample {
+    public static void main(String[] args) {
+        int a=5;
+        int result=(~a+1)*2;
+        System.out.println("Result:" +result);
+    }
+}
