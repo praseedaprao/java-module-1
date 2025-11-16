f1ee53a9c82b06684c9b870862d594cfa84acfe1
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:31:18 +0530

    Create logicalandexample.java

---
+++ java module 1/java module 2/logicalandexample.java
@@ -0,0 +1,13 @@
+public class LogicalAndExample {
+    public static void main(String[] args) {
+        int age=25;
+        boolean hasLicense=true;
+
+        if(age>18 && hasLicense){
+            System.out.println("Eligible to drive");
+        }
+        else{
+            System.out.println("Not Eligible to write");
+        }
+    }
+}
