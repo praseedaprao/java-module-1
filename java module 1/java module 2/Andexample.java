31b988d84206f1ebb9afb93e32e6e2d5a2ec950f
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:18:48 +0530

    Update Andexample.java

--- java module 1/java module 2/Andexample.java
+++ java module 1/java module 2/Andexample.java
@@ -1,1 +1,8 @@
-
+public class AndExample {
+    public static void main(String[] args) {
+        int a=12;
+        int b=10;
+        int result = a&b;
+        System.out.println("a&b=" +result);
+    }
+}
