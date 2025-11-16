ad14af9fbb93b4e0f875f61be98550472211f59a
praseedaprao <praseedarao4@gmail.com>
2025-11-16 20:36:13 +0530

    Create modulesex.java

---
+++ java module 1/java module 2/modulesex.java
@@ -0,0 +1,7 @@
+public class ModuleSex {
+    public static void main(String[] args) {
+        int a=25,b=4;
+        int remainder = a%b;
+        System.out.println("Reminder :"+remainder);
+    }
+}
